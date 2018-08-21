455b344b9de857e01cab364ea5e5d3060e2f3011
jair <vectormx@V0000671.vectoritcgroup.com>
2018-08-21 09:13:04 -0500

    inicio sesion

--- src/main/java/com/vector/controller/InicioSesionController.java
+++ src/main/java/com/vector/controller/InicioSesionController.java
@@ -6,10 +6,8 @@ package com.vector.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
